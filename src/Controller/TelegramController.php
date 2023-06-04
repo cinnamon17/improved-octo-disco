1d7945ce4d279129028d58b2f435ddf0973d4eca
Nelson Moncada <juniormoncada17@gmail.com>
2023-06-04 20:05:31 +0200

    initial telegram endpoint

--- src/Controller/TelegramController.php
+++ src/Controller/TelegramController.php
@@ -4,16 +4,22 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class TelegramController extends AbstractController
 {
     #[Route('/telegram', name: 'app_telegram')]
-    public function index(): JsonResponse
+    public function index(Request $request, HttpClientInterface $client): JsonResponse
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/TelegramController.php',
-        ]);
+        $update = json_decode($request->getContent(), true);
+        $chat_id = $update['message']['chat']['id'];
+
+        $response = $client->request('GET','https://api.telegram.org/bot1898926696:AAHyKmf02EF25EPupvGiotboowP6p8i8E6I/sendMessage', [
+                                    'json' => ['chat_id' => $chat_id, 'text' => 'hello']]);
+
+        return $this->json($response);
+
     }
 }
