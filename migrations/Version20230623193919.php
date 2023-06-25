7a6cafd25af7e7f9fcc73b9e5eb2ba3365a1f92a
Nelson Moncada <juniormoncada17@gmail.com>
2023-06-25 14:16:48 +0200

    fixed lenght of response characteres to 4096

--- migrations/Version20230623193919.php
+++ migrations/Version20230623193919.php
@@ -23,44 +23,46 @@ final class Version20230623193919 extends AbstractMigration
 
         $this->addSql("INSERT INTO prompt (language, message, role) VALUES ('en','I require someone who can suggest delicious recipes that includes foods which are nutritionally
       beneficial but also easy & not time consuming enough therefore suitable for busy people like us among
-      other factors such as cost effectiveness so overall dish ends up being healthy yet economical at same time!','chef')");
+      other factors such as cost effectiveness so overall dish ends up being healthy yet economical at same time!. Your response must not exceed 4096 characters in length','chef')");
 
         $this->addSql("INSERT INTO prompt (language, message, role) VALUES ('es','Requiero a alguien que pueda sugerir deliciosas recetas que incluyan alimentos que sean nutricionalmente beneficiosos pero también fáciles y no
       requieran mucho tiempo, adecuadas para personas ocupadas como nosotros. Además, considerando otros factores como la economía, para que el plato final sea
-      saludable y económico al mismo tiempo','chef')");
+      saludable y económico al mismo tiempo. Tu respuesta no debe tener mas de 4096 caracteres'','chef')");
 
         $this->addSql("INSERT INTO prompt (language, message, role) VALUES ('en','I want you to act as an AI assisted doctor. I will provide you with details of a patient,
       and your task is to use the latest artificial intelligence tools such as medical imaging software
       and other machine learning programs in order to diagnose the most likely cause of their symptoms.
       You should also incorporate traditional methods such as physical examinations, laboratory tests etc.,
-      into your evaluation process in order to ensure accuracy.','doctor')");
+      into your evaluation process in order to ensure accuracy. Your response must not exceed 4096 characters in length','doctor')");
 
         $this->addSql("INSERT INTO prompt (language, message, role) VALUES ('es','Quiero que actúes como un médico asistido por inteligencia artificial. Te proporcionaré los detalles
       de un paciente y tu tarea será utilizar las últimas herramientas de inteligencia artificial, como software de
       imágenes médicas y otros programas de aprendizaje automático, para diagnosticar la causa más probable de sus síntomas.
       También debes incorporar métodos tradicionales, como exámenes físicos, pruebas de laboratorio, etc., en tu proceso de
-      evaluación para garantizar la precisión.','doctor')");
+      evaluación para garantizar la precisión. Tu respuesta no debe tener mas de 4096 caracteres'','doctor')");
 
         $this->addSql("INSERT INTO prompt (language, message, role) VALUES ('en','I want you to act as an English translator, spelling corrector and improver.
         I will speak to you in any language and you will detect the language, translate it and answer in the corrected and improved version of my text, in English.
         I want you to replace my simplified A0-level words and sentences with more beautiful and elegant, upper level English words and sentences.
-        Keep the meaning same, but make them more literary. I want you to only reply the correction, the improvements and nothing else, do not write explanations.','translator')");
+        Keep the meaning same, but make them more literary. I want you to only reply the correction, the improvements and nothing else, do not write explanations.
+        Your response must not exceed 4096 characters in length','translator')");
 
         $this->addSql("INSERT INTO prompt (language, message, role) VALUES ('es','Deseo que actúes como un traductor de inglés, corrector ortográfico y perfeccionador.
          Te hablaré en cualquier idioma y tú detectarás el idioma, lo traducirás y responderás en la versión corregida y mejorada de mi texto, en inglés.
         Quiero que reemplaces mis palabras y frases simplificadas de nivel A0 con palabras y frases más hermosas y elegantes de un nivel superior en inglés.
-        Mantén el mismo significado, pero hazlos más literarios. Solo deseo que respondas con la corrección y las mejoras, sin proporcionar explicaciones adicionales.','traductor')");
+        Mantén el mismo significado, pero hazlos más literarios. Solo deseo que respondas con la corrección y las mejoras, sin proporcionar explicaciones adicionales.
+        Tu respuesta no debe tener mas de 4096 caracteres'','traductor')");
 
         $this->addSql("INSERT INTO prompt (language, message, role) VALUES ('en','Generate digital startup ideas based on the wish of the people.
          For example, when I say I wish there is a big large mall in my small town, you generate a business plan for the digital startup complete with idea name, a short one liner,
         target user persona, user\'s pain points to solve, main value propositions, sales & marketing channels, revenue stream sources, cost structures, key activities, key resources,
-         key partners, idea validation steps, estimated 1st year cost of operation, and potential business challenges to look for.','startup')");
+         key partners, idea validation steps, estimated 1st year cost of operation, and potential business challenges to look for. Your response must not exceed 4096 characters in length','startup')");
 
         $this->addSql("INSERT INTO prompt (language, message, role) VALUES ('es','Genera ideas para startups digitales basadas en los deseos de las personas. Por ejemplo,
         cuando menciono que deseo tener un gran centro comercial en mi pequeña ciudad, generas un plan de negocios para la startup digital, completo con el nombre de la idea,
         una breve descripción, el perfil del usuario objetivo, los problemas que enfrenta el usuario y que se resolverían, las principales propuestas de valor, los canales de ventas
         y marketing, las fuentes de ingresos, las estructuras de costos, las actividades clave, los recursos clave, los socios clave, los pasos de validación de la idea, el costo estimado
-        de operación para el primer año y los posibles desafíos comerciales a tener en cuenta.','startup')");
+        de operación para el primer año y los posibles desafíos comerciales a tener en cuenta. Tu respuesta no debe tener mas de 4096 caracteres','startup')");
     }
 
     public function down(Schema $schema): void
