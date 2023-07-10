f91eb75db2979469dd7baf986d8dc78f6e7215eb
Nelson Moncada <juniormoncada17@gmail.com>
2023-07-10 23:52:10 +0200

    installer symfony process component

--- src/Controller/ChessApiController.php
+++ src/Controller/ChessApiController.php
@@ -4,16 +4,18 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ChessApiController extends AbstractController
 {
     #[Route('/chess/api', name: 'app_chess_api')]
-    public function index(): JsonResponse
+    public function index(Request $request): JsonResponse
     {
+
+
         return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/ChessApiController.php',
+
         ]);
     }
 }
