fbcb2bd89a4faed1d6212fd498236ba5acb8b828
Nelson Moncada <juniormoncada17@gmail.com>
2023-07-11 00:24:33 +0200

    dummy implementation of stockfish

--- src/Controller/ChessApiController.php
+++ src/Controller/ChessApiController.php
@@ -5,6 +5,8 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Process\Exception\ProcessFailedException;
+use Symfony\Component\Process\Process;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ChessApiController extends AbstractController
@@ -13,9 +15,27 @@ class ChessApiController extends AbstractController
     public function index(Request $request): JsonResponse
     {
 
+        $process = new Process(['../bin/stockfish-ubuntu-x86-64-avx2']);
 
-        return $this->json([
 
-        ]);
+        while ($process->isRunning()) {
+            // waiting for process to finish
+        }
+
+        if(!$process->isSuccessful()){
+            throw new ProcessFailedException($process);
+        }
+
+        $process->setInput('position startpos moves e2e4 e7e5 g1f3 g8f6');
+        $process->setInput('go depth 10');
+
+        $process->start();
+
+        while ($process->isRunning()) {
+            // waiting for process to finish
+        }
+
+        $output = $process->getOutput();
+        return $this->json(['result' => $output]);
     }
 }
