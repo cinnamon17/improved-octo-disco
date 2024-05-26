cc0ac08cbd166ba2e84db741ea98cd1011d8bed3
Nelson Moncada <juniormoncada17@gmail.com>
2024-05-27 00:28:34 +0200

    fix broken test

--- tests/Controller/BookControllerTest.php
+++ tests/Controller/BookControllerTest.php
@@ -2,13 +2,15 @@
 
 namespace App\Tests\Controller;
 
+use ApiPlatform\Symfony\Bundle\Test\ApiTestCase;
+
 class BookControllerTest extends ApiTestCase
 {
     public function testSomething(): void
     {
-        $response = static::createClient()->request('GET', '/');
+        $response = static::createClient()->request('GET', '/api/books');
 
         $this->assertResponseIsSuccessful();
-        $this->assertJsonContains(['@id' => '/']);
+        $this->assertJsonContains(['@id' => '/api/books']);
     }
 }
