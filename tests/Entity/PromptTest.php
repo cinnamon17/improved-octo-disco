08446d3828908d406abe53d58b1c7625d1adc509
Nelson Moncada <juniormoncada17@gmail.com>
2024-09-19 21:48:04 +0200

    add test for Prompt entity

---
+++ tests/Entity/PromptTest.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Tests\Entity;
+
+use App\Entity\Prompt;
+use DateTime;
+use DateTimeImmutable;
+use PHPUnit\Framework\TestCase;
+
+class PromptTest extends TestCase
+{
+    public function testGetId(): void
+    {
+        $prompt = new Prompt();
+        $prompt->setId(1);
+        $this->assertEquals(1, $prompt->getId());
+    }
+
+    public function testGetLanguage(): void
+    {
+        $prompt = new Prompt();
+        $prompt->setLanguage('es');
+
+        $this->assertEquals('es', $prompt->getLanguage());
+    }
+
+
+    public function testGetMessage(): void
+    {
+        $prompt = new Prompt();
+        $prompt->setMessage("hello world");
+
+        $this->assertEquals('hello world', $prompt->getMessage());
+    }
+
+    public function testGetCreatedAt(): void
+    {
+        $prompt = new Prompt();
+        $date = new DateTimeImmutable();
+        $prompt->setCreatedAt($date);
+        $this->assertEquals($date, $prompt->getCreatedAt());
+    }
+
+    public function testGetUpdatedAt(): void
+    {
+
+        $prompt = new Prompt();
+        $date = new DateTimeImmutable();
+        $prompt->setCreatedAt($date);
+
+        $this->assertEquals($date, $prompt->getCreatedAt());
+    }
+
+    public function testUpdatedTimestamps(): void
+    {
+
+        $prompt = new Prompt();
+        $prompt->updatedTimestamps();
+
+        $this->assertInstanceOf(DateTimeImmutable::class, $prompt->getUpdatedAt());
+    }
+
+    public function testGetRole(): void
+    {
+        $prompt = new Prompt();
+        $prompt->setRole('doctor');
+
+        $this->assertEquals('doctor', $prompt->getRole());
+    }
+}
