1cd31a54b2195c9ba1d9e8de4b70ebdca9dae948
Nelson Moncada <juniormoncada17@gmail.com>
2025-10-19 14:07:42 +0200

    refactor: build the serialized request and delegate

--- src/Controller/TelegramController.php
+++ src/Controller/TelegramController.php
@@ -2,24 +2,39 @@
 
 namespace App\Controller;
 
-use App\Service\TelegramBotUpdate;
+use App\Dto\TelegramBotUpdate;
 use App\Service\TelegramRouter;
+use App\Service\UpdateSerializer;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\Routing\Annotation\Route;
 
 class TelegramController extends AbstractController
 {
     private TelegramRouter $router;
+    private UpdateSerializer $serializer;
+    private RequestStack $requestStack;
 
-    public function __construct(TelegramRouter $router)
+    public function __construct(TelegramRouter $router, UpdateSerializer $serializer, RequestStack $requestStack)
     {
-        $this->router= $router;
+        $this->router = $router;
+	$this->serializer = $serializer;
+	$this->requestStack = $requestStack;
     }
 
     #[Route('/telegram', name: 'app_telegram', methods: 'post')]
-    public function index(TelegramBotUpdate $update): JsonResponse
+    public function index(): JsonResponse
     {
-	return $this->router->handle($update);
+	$requestContent = $this->requestStack->getCurrentRequest()->getContent();
+	if (empty($requestContent)) {
+            return new JsonResponse(['status' => 'ignored'], 200);
+        }
+
+	$updateDto = $this->serializer->deserialize($requestContent);
+
+	$telegramBotUpdate = new TelegramBotUpdate($updateDto);
+
+	return $this->router->handle($telegramBotUpdate);
     }
 }
